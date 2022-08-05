60c9292374b38b0275067e3549874504ab269b8f
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-05 20:27:45 +0500

    все, игра работает

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 
-function one(x) {
+function one() {
+    function getRandomIntInclusive(min, max) {
+        min = 1;
+        max = 100;
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+    const x = getRandomIntInclusive();
+console.log(x);
+
     function two() {
         const a = +prompt("Введите число");
-        if (a == 0) {
-            confirm('Game ower');
-            two();
-        }else
-        if (a < x) {
-            alert('Ваше число меньше загаданного, введите другое число');
-            two();
-        }else
-        if (a > x) {
-            alert('Ваше число больше загаданного, введите другое число');
-            two();
-        }
-        else{
-            alert('Ответ верный')
-            return
+        switch (true) {
+            case a == 0: confirm('Game ower'); 
+                return;
+            case a < x: alert('Ваше число меньше загаданного, введите другое число');
+                two(); 
+                return;
+            case a > x: alert('Ваше число больше загаданного, введите другое число');
+                two();
+                return;
+            default:
+                alert('Ответ верный')
+                return;
         }
     }
     two();
 }
-one(80);
+one();
